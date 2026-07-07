09d176b6068e0b3cb7916d90a0de86a25986b866
Manvisingh24 <manvichauhan736@gmail.com>
2026-07-07 18:50:52 +0530

    Implemented addStudent function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,48 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <string>
+
 using namespace std;
+
+class Student{
+    public:
+        int rollNo;
+        string name;
+        int age;
+        string course;
+        float marks;
+};
+
+vector<Student> students;
+
+void addStudent()
+{
+    Student s;
+
+    cout << "Enter Roll Number: ";
+    cin >> s.rollNo;
+
+    cin.ignore();
+
+    cout<<"Enter Name:";
+    getline(cin, s.name);
+
+    cout<<"Enter Age:";
+    cin>>s.age;
+
+    cin.ignore();
+
+    cout<<"Enter Course:";
+    getline(cin, s.course);
+
+    cout<<"Enter Marks:";
+    cin>>s.marks;
+
+    students.push_back(s);
+
+    cout<<"\nStudent Added Successfully!\n";
+}
 int main(){
-    cout<<"Welcome to Smart Student Management System";
+    addStudent();
     return 0;
+}
