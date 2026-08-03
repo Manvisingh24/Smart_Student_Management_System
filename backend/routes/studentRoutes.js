8b7e2847abd3fc8c0d1f066c08c3ee3f4447ce57
Manvisingh24 <manvichauhan736@gmail.com>
2026-08-04 00:03:17 +0530

    Implement role-based authorization

--- backend/routes/studentRoutes.js
+++ backend/routes/studentRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const authMiddleware = require("../middleware/authMiddleware");
+const authorizeRole = require("../middleware/authorizeRole");
 
 const {
     getAllStudents,
@@ -11,24 +12,20 @@ const {
     deleteStudent
 } = require("../controllers/studentController");
 
-
 // GET all students
 router.get("/", authMiddleware, getAllStudents);
 
 // POST a new student
-router.post("/", createStudent);
-
+router.post("/", authMiddleware, authorizeRole("admin"), createStudent);
 
 // GET student by roll number
-router.get("/:rollNo", getStudentByRollNo);
-
+router.get("/:rollNo", authMiddleware, getStudentByRollNo);
 
 // PUT update student
-router.put("/:rollNo", updateStudent);
-
+router.put("/:rollNo", authMiddleware, authorizeRole("admin"), updateStudent);
 
 // DELETE student
-router.delete("/:rollNo", deleteStudent);
+router.delete("/:rollNo", authMiddleware, authorizeRole("admin"), deleteStudent);
 
 
 module.exports = router;
